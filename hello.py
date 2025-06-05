ec6ab122078740343fb05605036b523bdbc5f4f8
Jean Cleber Correia <CS008666@MinhaTI.com.br>
2025-06-04 21:29:38 -0300

    Atualizacao do Pipeline e Codigo Python

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # hello.py
+# Authorization: Basic amVhbjphYmNAMTIz
 def main():
     print("Meu primeiro codigo Python + GitHub Action!")
     print("\033[44mVersao Azul\033[0m")
-
 if __name__ == "__main__":
     main()
